3fc0882521f894cb532a0877f5d455b94614b61c
Kevetic <kevetic@outlook.com>
2019-10-31 21:15:00 -0500

    made some changes

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,68 +1,78 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
-$('#hidden-slidek').hide();
-$('#hidden-slideE').hide();
-$('#hidden-slideV').hide();
-$('#hidden-slideI').hide();
-$('#hidden-slideN').hide();
+    $('#hidden-slidek').hide();
+    $('#hidden-slideE').hide();
+    $('#hidden-slideV').hide();
+    $('#hidden-slideI').hide();
+    $('#hidden-slideN').hide();
 
-// Slide "K"
-$('#slidek').mouseenter(function () {
-    setTimeout(function(){
-    $('#hidden-slidek').fadeIn(1000);
-    $('#slide-titlek').text('Kevin Le');
-}).fadeIn(3000);
-});
-$('#slidek').mouseleave(function (){
-    $('#hidden-slidek').hide(1000);
-    $('#slide-titlek').text('K');
-});
+    // Slide "K"
+    $('#slidek').mouseenter(function () {
+        setTimeout(function () {
+            $('#slideimgk').hide();
+            $('#hidden-slidek').fadeIn(1000);
+            $('#slide-titlek').text('Kevin Le');
+        }).fadeIn(3000);
+    });
+    $('#slidek').mouseleave(function () {
+        $('#slideimgk').show();
+        $('#hidden-slidek').hide(1000);
+        $('#slide-titlek').text('K');
+    });
 
 
-// Slide "E"
-$('#slideE').mouseenter(function () {
-    setTimeout(function(){
-    $('#hidden-slideE').fadeIn(3000);
-    $('#slide-titleE').text('Experience');
-}).fadeIn(3000);
-});
-$('#slideE').mouseleave(function (){
-    $('#hidden-slideE').hide(1000);
-    $('#slide-titleE').text('E')
-});
-// Slide "V"
-$('#slideV').mouseenter(function () {
-    setTimeout(function(){
-    $('#hidden-slideV').fadeIn(3000);
-    $('#slide-titleV').text('Values');
-}).fadeIn(3000);
-});
-$('#slideV').mouseleave(function (){
-    $('#hidden-slideV').hide(1000);
-    $('#slide-titleV').text('V');
-})
-// Slide; "I"
-$('#slideI').mouseenter(function () {
-    setTimeout(function(){
-    $('#hidden-slideI').fadeIn(3000);
-    $('#slide-titleI').text('Interest');
-}).fadeIn(3000);
-})
-$('#slideI').mouseleave(function (){
-    $('#hidden-slideI').hide(1000);
-    $('#slide-titleI').text('I');
-});
+    // Slide "E"
+    $('#slideE').mouseenter(function () {
+        setTimeout(function () {
+            $('#slideimge').hide();
+            $('#hidden-slideE').fadeIn(3000);
+            $('#slide-titleE').text('Experience');
+        }).fadeIn(3000);
+    });
+    $('#slideE').mouseleave(function () {
+        $('#slideimge').show();
+        $('#hidden-slideE').hide(1000);
+        $('#slide-titleE').text('E')
+    });
+    // Slide "V"
+    $('#slideV').mouseenter(function () {
+        setTimeout(function () {
+            $('#slideimgv').hide();
+            $('#hidden-slideV').fadeIn(3000);
+            $('#slide-titleV').text('Values');
+        }).fadeIn(3000);
+    });
+    $('#slideV').mouseleave(function () {
+        $('#slideimgv').show();
+        $('#hidden-slideV').hide(1000);
+        $('#slide-titleV').text('V');
+    })
+    // Slide; "I"
+    $('#slideI').mouseenter(function () {
+        setTimeout(function () {
+            $('#slideimgi').hide();
+            $('#hidden-slideI').fadeIn(3000);
+            $('#slide-titleI').text('Interest');
+        }).fadeIn(3000);
+    })
+    $('#slideI').mouseleave(function () {
+        $('#slideimgi').show();
+        $('#hidden-slideI').hide(1000);
+        $('#slide-titleI').text('I');
+    });
 
-// Slide "N"
-$('#slideN').mouseenter(function () {
-    setTimeout(function(){
-    $('#hidden-slideN').fadeIn(3000);
-    $('#slide-titleN').text('Next?');
-}).fadeIn(3000);
-});
-$('#slideN').mouseleave(function (){
-    $('#hidden-slideN').hide(1000);
-    $('#slide-titleN').text('N');
-});
+    // Slide "N"
+    $('#slideN').mouseenter(function () {
+        setTimeout(function () {
+            $('#slideimgn').hide();
+            $('#hidden-slideN').fadeIn(3000);
+            $('#slide-titleN').text('Next?');
+        }).fadeIn(3000);
+    });
+    $('#slideN').mouseleave(function () {
+        $('#slideimgn').show();
+        $('#hidden-slideN').hide(1000);
+        $('#slide-titleN').text('N');
+    });
 
 })
